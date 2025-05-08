b0efe43fbbede89532f7ec819d3764c72067b140
adityabhati05 <aditya.bhati@unthinkable.co>
2025-05-08 16:34:31 +0530

    initial commit

---
+++ src/main/java/com/flexit/api_gateway/config/IniConfiguration.java
@@ -0,0 +1,17 @@
+package com.flexit.api_gateway.config;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IniConfiguration {
+
+    @Bean
+    public org.apache.commons.configuration.Configuration getIniConfig(@Autowired ApplicationArguments applicationArguments) throws ConfigurationException {
+        return new PropertiesConfiguration(applicationArguments.getOptionValues("Dpath").getFirst());
+    }
+}
---
+++ src/main/java/com/flexit/api_gateway/config/SpringCloudGatewayRouting.java
@@ -0,0 +1,17 @@
+package com.flexit.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudGatewayRouting {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("flexit-auth-backend", r -> r.path("/flexit/user/**")
+                        .uri("http://localhost:8091")).build();
+    }
+}
